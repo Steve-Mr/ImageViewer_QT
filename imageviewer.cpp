50198e6b371a10e5b0c19f2ac68dfac3dc2d7d99
maary <dicedsteve@gmail.com>
2022-06-19 23:21:55 +0800

    init(partially)

---
+++ imageviewer.cpp
@@ -0,0 +1,120 @@
+#include "imageviewer.h"
+
+#include <QApplication>
+#include <QClipboard>
+#include <QColorSpace>
+#include <QDir>
+#include <QFileDialog>
+#include <QImageReader>
+#include <QImageWriter>
+#include <QLabel>
+#include <QMenuBar>
+#include <QMessageBox>
+#include <QMimeData>
+#include <QPainter>
+#include <QScreen>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QStandardPaths>
+#include <QStatusBar>
+
+#if defined(QT_PRINTSUPPORT_LIB)
+#  include <QtPrintSupport/qtprintsupportglobal.h>
+
+#  if QT_CONFIG(printdialog)
+#    include <QPrintDialog>
+#  endif
+#endif
+
+ImageViewer::ImageViewer(QWidget *parent)
+    :QMainWindow(parent), imageLabel(new QLabel)
+    , scrollArea(new QScrollArea)
+{
+    imageLabel->setBackgroundRole(QPalette::Base);
+    imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    imageLabel->setScaledContents(true);
+
+    scrollArea->setBackgroundRole(QPalette::Base);
+    scrollArea->setWidget(imageLabel);
+    scrollArea->setVisible(false);
+    setCentralWidget(scrollArea);
+
+    createActions();
+
+    resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
+}
+
+
+static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+{
+    static bool firstDialog = true;
+
+    if(firstDialog){
+        firstDialog = false;
+        const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+        dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+    }
+
+    QStringList mimeTypeFilters;
+    const QByteArrayList supportedMimeTypes = acceptMode == QFileDialog::AcceptOpen
+            ? QImageReader::supportedMimeTypes() : QImageWriter::supportedMimeTypes();
+    for(const QByteArray &mimeTypeName: supportedMimeTypes)
+        mimeTypeFilters.append(mimeTypeName);
+    mimeTypeFilters.sort();
+    dialog.setMimeTypeFilters(mimeTypeFilters);
+    dialog.selectMimeTypeFilter("image/jpeg");
+    dialog.setAcceptMode(acceptMode);
+    if(acceptMode == QFileDialog::AcceptSave)
+        dialog.setDefaultSuffix("jpg");
+}
+
+
+void ImageViewer::open()
+{
+    QFileDialog dialog(this, tr("open file"));
+    initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
+
+    while(dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().constFirst()));
+}
+
+
+bool ImageViewer::loadFile(const QString &fileName)
+{
+    QImageReader reader(fileName);
+    reader.setAutoTransform(true);
+    const QImage newImage = reader.read();
+    if(newImage.isNull()){
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("cannot load %1: %2")
+                                 .arg(QDir::toNativeSeparators(fileName), reader.errorString()));
+        return false;
+    }
+
+    setImage(newImage);
+
+    setWindowFilePath(fileName);
+
+    const QString message = tr("Opened \%1\", %2x%3, Depth: %4")
+            .arg(QDir::toNativeSeparators(fileName)).arg(image.width()).arg(image.height()).arg(image.depth());
+    statusBar()->showMessage(message);
+    return true;
+}
+
+
+void ImageViewer::setImage(const QImage &newImage)
+{
+    image = newImage;
+    if(image.colorSpace().isValid())
+        image.convertToColorSpace(QColorSpace::SRgb);
+    imageLabel->setPixmap(QPixmap::fromImage(image));
+
+    scaleFactor = 1.0;
+
+    scrollArea->setVisible(true);
+    printAct->setEnabled(true);
+    fitToWindowAct->setEnabled(true);
+    updateActions();
+
+    if(!fitToWindowAct->isChecked())
+        imageLabel->adjustSize();
+}
